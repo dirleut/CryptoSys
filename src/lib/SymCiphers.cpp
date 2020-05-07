e2fdc7da964dfd66281a7f86e31916999c1cea6f
k.fokin <aallhhnn@gmail.com>
2020-05-07 17:28:51 +0300

    Добавляем подбор ключа для шифра Цезаря

--- src/lib/SymCiphers.cpp
+++ src/lib/SymCiphers.cpp
@@ -2,8 +2,6 @@
 #include <algorithm>
 #include <map>
 
-#include <iostream>
-
 std::map<char, double> english_frequencies = {
     {'A', 0.086}, {'B', 0.014}, {'C', 0.028}, {'D', 0.028},
     {'E', 0.130}, {'F', 0.029}, {'G', 0.010}, {'H', 0.053},
@@ -74,25 +72,10 @@ std::string scytale(const std::string& msg, short shift)
 bool isPlainEnglishText(const std::string& text)
 {
     double border = 44.3;
-    /*
-    std::map<char, uint32_t> letters = {
-        {'A', 0}, {'B', 0}, {'C', 0}, {'D', 0},
-        {'E', 0}, {'F', 0}, {'G', 0}, {'H', 0},
-        {'I', 0}, {'J', 0}, {'K', 0}, {'L', 0},
-        {'M', 0}, {'N', 0}, {'O', 0}, {'P', 0},
-        {'Q', 0}, {'R', 0}, {'S', 0}, {'T', 0},
-        {'U', 0}, {'V', 0}, {'W', 0}, {'X', 0},
-        {'Y', 0}, {'Z', 0}
-    };
-    */
     std::map<char, uint32_t> letters;
     for (size_t i = 0; i < text.size(); ++i)
     {
         ++letters[text[i]];
-    }
-    for (auto it = letters.begin(); it != letters.end(); ++it)
-    {
-        std::cout << it->first << "->" << it->second << std::endl;
     }
     
     double chi_squared = 0.0;
